7a9b1410375ff203efe5edfcaa08fd3ae01a8e97
Dmitrijs Minajevs <dmitrijs.minajevs@outlook.com>
2019-06-27 10:20:04 +0300

    Tab Dropdown on operation add modal

--- src/components/operations/OperationAddDialog.tsx
+++ src/components/operations/OperationAddDialog.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react'
-import { WithStyles, createStyles, Theme, withStyles } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
 
 import { Operation } from 'context/operation/types'
 import { context } from 'context/operation/operationAddModal'
 import { context as operationsContext } from 'context/operation/operations'
-
-import { MappedPartial } from 'utils'
+import { context as tabsContext } from 'context/tab/tabs'
+import { Tab } from 'context/tab/types'
 
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
@@ -15,12 +15,15 @@ import DialogContent from '@material-ui/core/DialogContent'
 import DialogContentText from '@material-ui/core/DialogContentText'
 import DialogTitle from '@material-ui/core/DialogTitle'
 
-const styles = (theme: Theme) => createStyles({
-    textField: {
+import TabDropdown from 'components/common/TabDropdown'
+
+const useStyles = makeStyles(theme => ({
+    field: {
         marginLeft: theme.spacing.unit,
         marginRight: theme.spacing.unit,
+        width: 150
     },
-})
+}))
 
 const defaultOperation: Operation = {
     id: 0,
@@ -53,18 +56,22 @@ const createInput = (state: Operation, setState: React.Dispatch<React.SetStateAc
         />
     }
 
-const OperationAddDialog: React.SFC<WithStyles<typeof styles>> = (props) => {
+const OperationAddDialog: React.SFC = props => {
     const [state, setState] = React.useState(defaultOperation)
 
-    const { classes } = props
+    const classes = useStyles()
     const store = React.useContext(context)
     const operationsStore = React.useContext(operationsContext)
+    const tabsStore = React.useContext(tabsContext)
 
     const handleSave = React.useCallback(async (operation: Operation) => {
         await operationsStore.add(operation)
-        operationsStore.getAll()
         store.closeModal()
         setState(defaultOperation)
+    }, [])
+
+    const handleTabChange = React.useCallback((key: keyof Operation) => (tab: Tab) => {
+        setState(prev => ({ ...prev, [key]: tab.id }))
     }, [])
 
     return (
@@ -78,12 +85,14 @@ const OperationAddDialog: React.SFC<WithStyles<typeof styles>> = (props) => {
                 <DialogContentText>
                     Please fill all the necessary fields
                 </DialogContentText>
-                {createInput(state, setState, classes.textField)('from')}
-                {createInput(state, setState, classes.textField)('to')}
-                {createInput(state, setState, classes.textField)('comment')}
-                {createInput(state, setState, classes.textField)('date')}
-                {createInput(state, setState, classes.textField)('currency')}
-                {createInput(state, setState, classes.textField)('amount')}
+                <TabDropdown name="From" elementProps={{ className: classes.field }}
+                    tabs={tabsStore.tabs} defaultValue={tabsStore.tabs[0]} onChange={handleTabChange('from')} />
+                <TabDropdown name="To" elementProps={{ className: classes.field }}
+                    tabs={tabsStore.tabs} defaultValue={tabsStore.tabs[0]} onChange={handleTabChange('to')} />
+                {createInput(state, setState, classes.field)('comment')}
+                {createInput(state, setState, classes.field)('date')}
+                {createInput(state, setState, classes.field)('currency')}
+                {createInput(state, setState, classes.field)('amount')}
             </DialogContent>
             <DialogActions>
                 <Button onClick={store.closeModal}>
@@ -97,4 +106,4 @@ const OperationAddDialog: React.SFC<WithStyles<typeof styles>> = (props) => {
     )
 }
 
-export default withStyles(styles)(OperationAddDialog)
+export default OperationAddDialog
